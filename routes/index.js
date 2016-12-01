b395c889d15e608f847ad8bad6622d507ed63602
Steve Sharp <zimsteve@live.com>
2016-12-01 16:17:01 +1300

    set-up some templates

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router()
+const getAllUsers = require('../db/db').getAllUsers
+const addUser = require('../db/db').addUser
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  getAllUsers()
+  .then((users) => {
+    res.json(users)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+});
+
+router.post('/', function(req, res) {
+  console.log(req.body);
+  addUser(req.body)
+  .then((data) => res.json(data))
+  .catch((err) => {
+    console.log(err)
+  })
+})
+
+module.exports = router;
